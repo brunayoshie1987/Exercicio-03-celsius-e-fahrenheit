a43b1e284e820cb40983230c6fb0d1420130aec6
unknown <bruna.yoshie@iventis.com.br>
2020-08-02 21:53:02 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Exercicio_3___Celsius_e_Fahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Informe a quantidade de Graus Celsius que deseja converter");
+
+                var celsius = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Informe a quantidade de Graus Fahrenheit que deseja converter");
+
+                var fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine(celsius + " graus Celsius em Fahrenheit é " + CelsiusToFahrenheit(celsius));
+
+                Console.WriteLine(fahrenheit + " graus Fahrenheit em Celsius é " + FahrenheitToCelsius(fahrenheit));
+
+                Console.ReadKey();
+            }
+
+             static double FahrenheitToCelsius(double valor)
+            {
+                //FORMULA RETIRADA DA CALCULADORA DO GOOGLE
+                var resultado = (valor - 32) * (5.0 / 9.0);
+
+                return resultado;
+            }
+
+            static double CelsiusToFahrenheit(double valor)
+            {
+                //FORMULA RETIRADA DA CALCULADORA DO GOOGLE
+                var resultado = (valor * (9.0 / 5.0)) + 32;
+
+                return resultado;
+            }
+        }
+    }
+    }
+
